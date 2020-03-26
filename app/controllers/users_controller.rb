eedf251725bd006d6ae344809a31c4dcabff8a7b
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-26 18:40:12 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def create
     @user = User.create(params[:user])
   end
+
+  private
+
+  def user_params
+    params.require('user').permit(:name, :password, :password_confirmation)
 end
