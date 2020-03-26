26d02688ccb7a4582574daf164b8205d9b15ba76
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-26 18:31:42 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     if params[:name]
       user = User.find_or_create_by(name: params[:name])
-      session[:user_id] = user.id 
+      session[:user_id] = user.id
     end
   end
 
