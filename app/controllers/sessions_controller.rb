5a645dfb973fd31f3f7d1af0251ca4ed139f42f3
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-26 18:31:39 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
+  
   def create
+    if params[:name]
+      user = User.find_or_create_by(name: params[:name])
+      session[:user_id] = user.id 
+    end
   end
 
   def destroy
