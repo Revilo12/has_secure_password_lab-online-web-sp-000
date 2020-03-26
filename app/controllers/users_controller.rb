a4423827ef0bd219863fd6f4e8f8b1259f5a2a36
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-26 18:47:16 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       @user = User.create(user_params)
       login(@user)
     else
-      redirect_to "/users/new"
+      redirect_to :new
     end
   end
 
