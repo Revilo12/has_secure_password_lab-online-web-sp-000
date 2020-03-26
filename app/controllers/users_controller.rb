b46bd0d69a664bc12ae29cd2a46ddabe5631b589
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-26 18:43:28 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
   def create
-    @user = User.create(user_params)
-    login(@user)
+    if passowords_match?
+      @user = User.create(user_params)
+      login(@user)
+    else
+      redirect_to "/users/new"
+    end
   end
 
   private
 
   def user_params
     params.require('user').permit(:name, :password, :password_confirmation)
+  end
+
+  def passowords_match?
+    params[:password] == params[:password_confirmation]
   end
 end
